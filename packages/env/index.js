6687a5640c52ce87c3a0188a2e488cac0762729b
Nikolay Lapshin <nlapshin1989@gmail.com>
2020-10-03 23:40:25 +0300

    Added environment register

--- packages/env/index.js
+++ packages/env/index.js
@@ -4,13 +4,17 @@ const DotEnv = require('dotenv');
 const ENV_PATH = path.resolve('./', '.env');
 const SECRETS_PATH = path.resolve('./', '.secrets');
 
-const env = DotEnv.config({ path: ENV_PATH }).parsed;
-const secrets = DotEnv.config({ path: SECRETS_PATH }).parsed;
+module.exports = {
+  extends() {
+    const env = DotEnv.config({ path: ENV_PATH }).parsed;
+    const secrets = DotEnv.config({ path: SECRETS_PATH }).parsed;
 
-Object.keys(env).forEach(function (key) {
-  process.env[key] = env[key];
-});
+    Object.keys(env).forEach(function (key) {
+      process.env[key] = env[key];
+    });
 
-Object.keys(secrets).forEach(function (key) {
-  process.env[key] = secrets[key];
-});
+    Object.keys(secrets).forEach(function (key) {
+      process.env[key] = secrets[key];
+    });
+  }
+}
