58427e5d7773969ee69cd146d28b4c01c601f5d9
Nikolay Lapshin <nlapshin1989@gmail.com>
2020-10-04 00:27:55 +0300

    Added test sample

---
+++ packages/database/test/sample.ts
@@ -0,0 +1,80 @@
+const list = [
+  {
+    "eng" : "start",
+    "rus" : "начинать(ся)",
+    "type" : "verb",
+    "section" : "stage",
+    "subsection" : "beginning",
+    "rate" : 100500.0,
+    "stats" : {
+        "failure" : 0.0,
+        "success" : 0.0,
+        "attempts" : 0.0
+    }
+  },
+  {
+    "eng" : "begin",
+    "rus" : "начинать(ся)",
+    "type" : "verb",
+    "section" : "stage",
+    "subsection" : "beginning",
+    "rate" : 100500.0,
+    "stats" : {
+        "failure" : 0.0,
+        "success" : 0.0,
+        "attempts" : 0.0
+    }
+  },
+  {
+    "eng" : "appear",
+    "rus" : "появляться",
+    "type" : "verb",
+    "section" : "stage",
+    "subsection" : "beginning",
+    "rate" : 100500.0,
+    "stats" : {
+        "failure" : 0.0,
+        "success" : 0.0,
+        "attempts" : 0.0
+    }
+  },
+  {
+    "eng" : "grow",
+    "rus" : "становиться",
+    "type" : "verb",
+    "section" : "stage",
+    "subsection" : "beginning",
+    "rate" : 100500.0,
+    "stats" : {
+        "failure" : 0.0,
+        "success" : 0.0,
+        "attempts" : 0.0
+    }
+  },
+  {
+    "eng" : "go on",
+    "rus" : "продолжать",
+    "type" : "verb",
+    "section" : "stage",
+    "subsection" : "continuation",
+    "rate" : 100500.0,
+    "stats" : {
+        "failure" : 0.0,
+        "success" : 0.0,
+        "attempts" : 0.0
+    }
+  },
+  {
+    "eng" : "skate",
+    "rus" : "кататься на коньках",
+    "type" : "verb",
+    "section" : "movement",
+    "subsection" : "ways_of_movement",
+    "rate" : 100500.0,
+    "stats" : {
+        "failure" : 0.0,
+        "success" : 0.0,
+        "attempts" : 0.0
+    }
+  }
+]
