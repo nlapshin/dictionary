9a2535d14fd2429fbd788769f28fff7cd8d549cd
Nikolay Lapshin <nlapshin1989@gmail.com>
2020-10-03 23:15:11 +0300

    Added env package

---
+++ packages/env/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const DotEnv = require('dotenv');
+
+const ENV_PATH = path.resolve('./', '.env');
+const SECRETS_PATH = path.resolve('./', '.secrets');
+
+const env = DotEnv.config({ path: ENV_PATH }).parsed;
+const secrets = DotEnv.config({ path: SECRETS_PATH }).parsed;
+
+Object.keys(env).forEach(function (key) {
+  process.env[key] = env[key];
+});
+
+Object.keys(secrets).forEach(function (key) {
+  process.env[key] = secrets[key];
+});
